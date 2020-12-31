46b8a8feefe52c3ba21df0a5d1d1b1da4d06d2cc
Ikraam <IbnDaanis@Gmail.com>
2020-12-31 12:08:29 +0000

    Added more grid fill-in features, used const for grid size, style changes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
-const container = document.querySelector('#container')
+const container = document.querySelector('#gridContainer')
+
+const gridSize = 16
 
 const createGrid = () => {
-  container.style.gridTemplateRows = 'repeat(100, 1fr)'
-  container.style.gridTemplateColumns = 'repeat(100, 1fr)'
-  for (let i = 0; i < 100 ** 2; i++) {
+  container.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+  for (let i = 0; i < gridSize ** 2; i++) {
     const cell = document.createElement('div')
     cell.classList.add('cell')
     container.appendChild(cell)
@@ -11,7 +13,16 @@ const createGrid = () => {
 }
 createGrid()
 const cells = document.querySelectorAll('.cell')
-console.log(cells.length)
+
+for (let i = gridSize - 1; i < cells.length; i++) {
+  if ((i + 1) % gridSize === 0) {
+    cells[i].style.borderRight = '1px solid #c7c7c7'
+  }
+}
+for (let i = cells.length - gridSize; i < cells.length; i++) {
+  cells[i].style.borderBottom = '1px solid #c7c7c7'
+}
+
 let clicking = false
 container.addEventListener('mousedown', e => {
   clicking = true
@@ -24,4 +35,9 @@ container.addEventListener('mousemove', e => {
   if (e.target.classList.contains('cell')) {
     clicking ? (e.target.style.background = 'red') : null
   }
+})
+container.addEventListener('click', e => {
+  clicking = true
+  clicking ? (e.target.style.background = 'red') : null
+  clicking = false
 })
